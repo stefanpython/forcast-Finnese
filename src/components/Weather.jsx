891f9d84ad6d2bed4afae9efae2d5323698bd385
stefanpython <stefanhtml@yahoo.com>
2023-10-18 20:05:15 +0300

    Display basic weather information based on location

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -10,7 +10,7 @@ function Weather() {
 
   const fetchWeather = () => {
     const apiKey = "3ba340eb358c48d08ae154329231810";
-    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=New York`;
+    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=London&days=7`;
 
     fetch(apiUrl)
       .then((response) => {
@@ -33,6 +33,47 @@ function Weather() {
   return (
     <div className="weather-container">
       <h1>Wheather fetch</h1>
+
+      {weatherData && (
+        <div>
+          <h2>Location: {weatherData.location.name}</h2>
+          <h3>Region: {weatherData.location.region}</h3>
+          <h3>Country: {weatherData.location.country}</h3>
+          <h3>Local time: {weatherData.location.localtime}</h3>
+          <h3>Temperature: {weatherData.current.temp_c}°C</h3>
+          <h3>Feels Like: {weatherData.current.feelslike_c}°C</h3>
+
+          <h3>Humidity: {weatherData.current.humidity}%</h3>
+          <h3>UV Index: {weatherData.current.uv}</h3>
+          <h3>Visibility: {weatherData.current.vis_km} km</h3>
+          <h3>
+            Wind: {weatherData.current.wind_dir} at{" "}
+            {weatherData.current.wind_kph} km/h
+          </h3>
+
+          <h3>Sunrise: {weatherData.forecast.forecastday[0].astro.sunrise}</h3>
+          <h3>Sunset: {weatherData.forecast.forecastday[0].astro.sunset}</h3>
+
+          <div>
+            <h3>
+              Chance of Rain:{" "}
+              {weatherData.forecast.forecastday[0].day.daily_chance_of_rain}%
+            </h3>
+            <h3>
+              Will It Rain?{" "}
+              {weatherData.forecast.forecastday[0].day.daily_will_it_rain
+                ? "Yes"
+                : "No"}
+            </h3>
+          </div>
+
+          <h3>Condition: {weatherData.current.condition.text}</h3>
+          <img
+            src={`https:${weatherData.current.condition.icon}`}
+            alt={weatherData.current.condition.text}
+          />
+        </div>
+      )}
     </div>
   );
 }
