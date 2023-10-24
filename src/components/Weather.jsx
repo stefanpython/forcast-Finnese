7cccabf18697a0f5b11e1829c9aa8327e0d45dd3
stefanpython <stefanhtml@yahoo.com>
2023-10-24 20:17:09 +0300

    Edit weather page layout

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -10,7 +10,7 @@ function Weather() {
 
   const fetchWeather = () => {
     const apiKey = "3ba340eb358c48d08ae154329231810";
-    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Berlin&days=7`;
+    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Vietnam&days=7`;
 
     fetch(apiUrl)
       .then((response) => {
@@ -40,62 +40,68 @@ function Weather() {
           </h2>
           <h4>{weatherData.location.localtime}</h4>
 
-          <h3>Temperature: {weatherData.current.temp_c}°C</h3>
-          <h3>Feels Like: {weatherData.current.feelslike_c}°C</h3>
+          <div className="temp-box">
+            <img
+              className="condition-icon"
+              src={`https:${weatherData.current.condition.icon}`}
+              alt={weatherData.current.condition.text}
+            />
+            <h3 className="current-temp"> {weatherData.current.temp_c}°C</h3>
+          </div>
+
+          <h3>{weatherData.current.condition.text}</h3>
+          <h4>Feels Like: {weatherData.current.feelslike_c}°C</h4>
+
+          <hr />
 
           <div className="info-box">
             <div className="box">
+              <span>Humidity:</span>
+              <h3>{weatherData.current.humidity}%</h3>
+            </div>
+
+            <div className="box">
+              <span>UV Index:</span>
+              <h3>{weatherData.current.uv}</h3>
+            </div>
+
+            <div className="box">
+              <span>Visibility:</span>
+              <h3>{weatherData.current.vis_km} km</h3>
+            </div>
+
+            <div className="box">
+              <span>Wind:</span>
               <h3>
-                <span>Humidity:</span> {weatherData.current.humidity}%
+                {weatherData.current.wind_dir} at {weatherData.current.wind_kph}{" "}
+                km/h
               </h3>
             </div>
 
             <div className="box">
-              <h3>
-                <span>UV Index:</span> {weatherData.current.uv}
-              </h3>
+              <span>Sunrise: </span>
+              <h3>{weatherData.forecast.forecastday[0].astro.sunrise}</h3>
             </div>
 
             <div className="box">
-              <h3>
-                <span>Visibility:</span> {weatherData.current.vis_km} km
-              </h3>
+              <span>Sunset:</span>{" "}
+              <h3>{weatherData.forecast.forecastday[0].astro.sunset}</h3>
             </div>
 
             <div className="box">
+              <span>Chance of Rain: </span>
               <h3>
-                <span>Wind:</span> {weatherData.current.wind_dir} at{" "}
-                {weatherData.current.wind_kph} km/h
-              </h3>
-            </div>
-
-            <div className="box">
-              <h3>
-                <span>Sunrise: </span>
-                {weatherData.forecast.forecastday[0].astro.sunrise}
-              </h3>
-            </div>
-
-            <div className="box">
-              <h3>
-                <span>Sunset:</span>{" "}
-                {weatherData.forecast.forecastday[0].astro.sunset}
-              </h3>
-            </div>
-
-            <div className="box">
-              <h3>
-                <span>Chance of Rain: </span>
                 {weatherData.forecast.forecastday[0].day.daily_chance_of_rain}%
               </h3>
             </div>
-          </div>
 
-          <h3>Condition: {weatherData.current.condition.text}</h3>
-          <img
-            src={`https:${weatherData.current.condition.icon}`}
-            alt={weatherData.current.condition.text}
-          />
+            <div className="box">
+              <h3>
+                <span> </span>
+                {weatherData.forecast.forecastday[0].astro.moon_phase}
+              </h3>
+            </div>
+          </div>
         </div>
       )}
     </div>
